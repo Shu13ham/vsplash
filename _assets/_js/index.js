1bcf62c1a4fe2c3c56e8e6e98de27c37b4b5eaf0
Shubham Shankar Sawant <75775348+Shu13ham@users.noreply.github.com>
2024-05-05 18:09:58 +0530

    hover animations

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const aside = document.querySelector("aside");
+  const filterWrapper = document.querySelector(".filter-wrapper");
+  const mainSection = document.querySelector(".main-section");
+  var hamburgerOpen = false;
 
+  hamburger.addEventListener('click', function(){
+    if(!hamburgerOpen){
+      aside.classList.add("hamburger-clicked");
+      filterWrapper.style.left = "28px";
+      mainSection.style.marginLeft = "278px";
+      hamburgerOpen = true;
+    }
+    else{
+      aside.classList.remove("hamburger-clicked");
+      filterWrapper.style.left = "100px";
+      mainSection.style.marginLeft = "350px";
+      hamburgerOpen = false;
+    }
+    
+  })
 });
